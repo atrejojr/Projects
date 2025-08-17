88d9f589d552dc49f6a5888e29edc81f089cccb1
Alberto Trejo <atrejojr@bu.edu>
2025-08-17 12:18:53 -0400

    laptop broke, saving progress

---
+++ Coding/Learning C_C++/3. Math/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>                       // enables math functions (sqrt, pow, etc.)
+
+int main(){
+
+    int x = 4;
+
+    // x = sqrt(x);
+    //x = pow(x, 4);
+    
+
+    printf("%d", x);
+
+    return 0;
+}
